783e732155c684d2aa660d7781e25dad282e667d
Sabbir Hossain Rizvi <rizvibd1100@gmail.com>
2026-05-22 21:17:18 +0600

    initialize Better Auth configuration with MongoDB adapter and JWT support

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -7,26 +7,25 @@ const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("ideavault");
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET || "kV9mPxR7nL3wQjY8tF2aHcU5dBgZ0sEi",
   database: mongodbAdapter(db, {
-    client
+    client,
   }),
   emailAndPassword: {
     enabled: true,
   },
   socialProviders: {
     google: {
-      clientId: process.env.GOOGLE_CLIENTID,
-      clientSecret: process.env.GOOGLE_SECRET
-    }
+      clientId: process.env.GOOGLE_CLIENTID || "dummy-client-id",
+      clientSecret: process.env.GOOGLE_SECRET || "dummy-secret",
+    },
   },
   session: {
     cookieCache: {
       enabled: true,
       strategy: "jwt",
-      maxAge: 7 * 24 * 60 * 60
-    }
+      maxAge: 7 * 24 * 60 * 60,
+    },
   },
-  plugins: [
-    jwt()
-  ]
+  plugins: [jwt()],
 });
